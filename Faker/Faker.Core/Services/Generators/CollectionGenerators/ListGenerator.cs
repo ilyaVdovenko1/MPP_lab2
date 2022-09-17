c4bcf143c46dc2b0f307723211a1f5f5a5475e41
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-17 17:46:19 +0300

    Added collections type generator.

---
+++ Faker/Faker.Core/Services/Generators/CollectionGenerators/ListGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Reflection;
+using Faker.Core.Exceptions;
+using Faker.Core.Interfaces;
+using Faker.Core.Models;
+
+namespace Faker.Core.Services.Generators.CollectionGenerators;
+
+public class ListGenerator : GeneratorBase
+{
+    private const int DefaultMinLength = 0;
+    private const int DefaultMaxLength = 100;
+    
+    private readonly Range<int> lengthRange;
+
+    public ListGenerator()
+    {
+        lengthRange = new Range<int>(DefaultMinLength, DefaultMaxLength);
+    }
+    
+    public ListGenerator(Range<int> lengthRange)
+    {
+        this.lengthRange = lengthRange;
+    }
+    public override object? Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        if (!CanGenerate(typeToGenerate))
+        {
+            base.Generate(typeToGenerate, context);
+        }
+        
+        var length = context.Random.Next(this.lengthRange.Minimum, this.lengthRange.Maximum + 1);
+        
+        var generationType = typeToGenerate.GetGenericArguments().FirstOrDefault() 
+                             ?? throw new WrongTypeToGenerateException($"This generator can not generate this type: {typeToGenerate}");
+        
+        var list = (IList?)Activator.CreateInstance(typeToGenerate)
+                   ?? throw new WrongTypeToGenerateException($"This generator can not generate this type: {typeToGenerate}");
+        
+        for (var i = 0; i < length - 1; i++)
+        {
+            list.Add(context.Faker.Create(generationType));
+            Thread.Sleep(1);
+        }
+
+        return list;
+    }
+
+    public override bool CanGenerate(Type type)
+    {
+        return type.GetInterfaces().Contains(typeof(IList));
+    }
+}
---
+++ Faker/Faker.Core/Services/Generators/CollectionGenerators/StringGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Faker.Core.Exceptions;
+using Faker.Core.Interfaces;
+using Faker.Core.Models;
+
+namespace Faker.Core.Services.Generators.CollectionGenerators;
+
+public class StringGenerator : GeneratorBase
+{
+    private const string DefaultAlphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+    private const int DefaultMinLength = 0;
+    private const int DefaultMaxLength = 100;
+    
+    private readonly IList<char> alphabet;
+    private readonly Range<int> lengthRange;
+
+    public StringGenerator()
+    {
+        this.alphabet = DefaultAlphabet.ToCharArray();
+        this.lengthRange = new Range<int>(DefaultMinLength, DefaultMaxLength);
+    }
+    
+    public StringGenerator(IList<char> alphabet, Range<int> lengthRange)
+    {
+        this.lengthRange = lengthRange;
+        this.alphabet = alphabet;
+    }
+    public override object? Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        if (!CanGenerate(typeToGenerate))
+        {
+            base.Generate(typeToGenerate, context);
+        }
+        
+        var length = context.Random.Next(this.lengthRange.Minimum, this.lengthRange.Maximum + 1);
+        
+        return new string(Enumerable.Repeat(this.alphabet, length)
+            .Select(s => s[context.Random.Next(this.alphabet.Count)]).ToArray());
+    }
+
+    public override bool CanGenerate(Type type)
+    {
+        return type == typeof(string);
+    }
+}
