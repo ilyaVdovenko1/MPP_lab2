93e5d72a98f5f551b61c5bf23da77f7d38da3fcd
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-17 17:48:01 +0300

    Added faker implementation.

---
+++ Faker/Faker.Core/Models/Faker.cs
@@ -0,0 +1,35 @@
+﻿using Faker.Core.Interfaces;
+
+namespace Faker.Core.Models;
+
+public class Faker : IFaker
+{
+    private readonly IValueGenerator generator;
+    private readonly Random random;
+    
+    public Faker(IValueGenerator generator)
+    {
+        this.generator = generator;
+        this.random = new Random();
+    }
+    
+    public T? Create<T>()
+    {
+        return (T?)Create(typeof(T));
+    }
+
+    public object? Create(Type type)
+    {
+        var generatorContext = new GeneratorContext(this.random, this);
+
+        return this.generator.Generate(type, generatorContext);
+    }
+
+    private static object? GetDefaultValue(Type type)
+    {
+        if (type.IsValueType)
+            return Activator.CreateInstance(type);
+        
+        return null;
+    }
+}
