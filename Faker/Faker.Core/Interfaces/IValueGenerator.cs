990d8b01093e789c502d294a65fe3abacdc42ba9
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-17 17:45:13 +0300

    Added value generator interface, that implements chain of responsibility pattern.

---
+++ Faker/Faker.Core/Interfaces/IValueGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Faker.Core.Models;
+
+namespace Faker.Core.Interfaces;
+
+public interface IValueGenerator
+{
+    public void SetNext(IValueGenerator generator);
+    
+    public object? Generate(Type typeToGenerate, GeneratorContext context);
+    
+    public bool CanGenerate(Type type);
+}
