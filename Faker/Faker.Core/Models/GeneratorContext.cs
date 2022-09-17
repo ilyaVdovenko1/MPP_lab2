0f5f4aaa135d63cd778994a8771bb829f7305350
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-17 17:47:21 +0300

    Added Generator Context dto.

---
+++ Faker/Faker.Core/Models/GeneratorContext.cs
@@ -0,0 +1,17 @@
+﻿using Faker.Core.Interfaces;
+
+namespace Faker.Core.Models;
+
+public class GeneratorContext
+{
+    public GeneratorContext(Random random, IFaker faker)
+    {
+        Random = random;
+        Faker = faker;
+    }
+    
+    public Random Random { get; }
+    
+    public IFaker Faker { get; }
+
+}
