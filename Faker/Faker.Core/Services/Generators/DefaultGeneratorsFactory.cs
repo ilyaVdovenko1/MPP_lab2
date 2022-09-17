59fe812013686cea5d31344aaad62d3ed3659560
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-17 17:46:45 +0300

    Added default generators factory.

---
+++ Faker/Faker.Core/Services/Generators/DefaultGeneratorsFactory.cs
@@ -0,0 +1,41 @@
+﻿using Faker.Core.Interfaces;
+using Faker.Core.Services.Generators.CollectionGenerators;
+using Faker.Core.Services.Generators.ValueTypesGenerators;
+
+namespace Faker.Core.Services.Generators;
+
+public static class DefaultGeneratorsFactory
+{
+    public static IValueGenerator CreateDefaultGenerator()
+    {
+        var generator = new BoolGenerator();
+        generator.SetNext(new CharGenerator());
+        generator.SetNext(new DoubleGenerator());
+        generator.SetNext(new IntegerGenerator());
+        generator.SetNext(new DateTimeGenerator());
+        generator.AddDefaultCollectionsGenerators();
+        
+        return generator;
+
+    }
+    
+    public static IValueGenerator AddDefaultCollectionsGenerators(this IValueGenerator generator)
+    {
+        generator.SetNext(new StringGenerator());
+        generator.SetNext(new ListGenerator());
+        
+        return generator;
+    }
+    
+    public static IValueGenerator AddPrimitiveGenerators(this IValueGenerator generator)
+    {
+        generator.SetNext(new BoolGenerator());
+        generator.SetNext(new CharGenerator());
+        generator.SetNext(new DoubleGenerator());
+        generator.SetNext(new IntegerGenerator());
+        generator.SetNext(new DateTimeGenerator());
+
+        return generator;
+    }
+    
+}
