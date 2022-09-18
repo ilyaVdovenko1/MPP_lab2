3f800d3c4c467dc9300fb2139ec151bf7c2b60aa
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-18 19:41:50 +0300

    Added user classes generator.

---
+++ Faker/Faker.Core/Services/Generators/UsersTypesGenerators/UserClassProcessor/ConstructorComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Faker.Core.Services.Generators.UsersTypesGenerators.UserClassProcessor;
+
+public class ConstructorComparer : IComparer<ConstructorInfo>
+{
+    public int Compare(ConstructorInfo? x, ConstructorInfo? y)
+    {
+        if (ReferenceEquals(x, y)) return 0;
+        if (ReferenceEquals(null, y)) return 1;
+        if (ReferenceEquals(null, x)) return -1;
+        
+        return x.GetParameters().Length.CompareTo(y.GetParameters().Length);
+    }
+}
---
+++ Faker/Faker.Core/Services/Generators/UsersTypesGenerators/UserClassProcessor/UserClassGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+using Faker.Core.Exceptions;
+using Faker.Core.Models;
+
+namespace Faker.Core.Services.Generators.UsersTypesGenerators.UserClassProcessor;
+
+public class UserClassGenerator : GeneratorBase
+{
+    private HashSet<Type> types;
+    public UserClassGenerator()
+    {
+        this.types = new HashSet<Type>();
+    }
+
+    public override object? Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        if (!CanGenerate(typeToGenerate))
+        {
+            return base.Generate(typeToGenerate, context);
+        }
+
+        if (this.types.Contains(typeToGenerate))
+        {
+            throw new CycledDependencyException();
+        }
+
+        this.types.Add(typeToGenerate);
+
+        var properties = typeToGenerate.GetProperties();
+        var propValues = properties.Where(propertyInfo => propertyInfo.CanWrite).ToDictionary(propertyInfo => propertyInfo, propertyInfo => context.Faker.Create(propertyInfo.PropertyType));
+
+        var constructors = typeToGenerate.GetConstructors();
+        var comparer = new ConstructorComparer();
+        Array.Sort(constructors, comparer);
+        object? obj = null;
+
+        foreach (var constructor in constructors)
+        {
+            var paramsTypes = constructor.GetParameters();
+
+            try
+            {
+                obj = constructor.Invoke(paramsTypes.Select(info => context.Faker.Create(info.ParameterType)).ToArray());
+                break;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        foreach (var propWithSetter in propValues)
+        {
+            propWithSetter.Key.SetValue(obj, propWithSetter.Value);
+        }
+
+        return obj;
+
+
+    }
+
+    public override bool CanGenerate(Type type)
+    {
+        return !type.IsAbstract && type.IsClass;
+    }
+}
