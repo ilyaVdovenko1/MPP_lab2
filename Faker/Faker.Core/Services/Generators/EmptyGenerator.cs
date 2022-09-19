8c293b6803084b88b6e54e4b69a5ead4358d26ea
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-19 15:59:42 +0300

    Added empty generator.

---
+++ Faker/Faker.Core/Services/Generators/EmptyGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Faker.Core.Models;
+
+namespace Faker.Core.Services.Generators;
+
+public class EmptyGenerator : GeneratorBase
+{
+    public override object? Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        return null;
+    }
+
+    public override bool CanGenerate(Type type)
+    {
+        return false;
+    }
+}
